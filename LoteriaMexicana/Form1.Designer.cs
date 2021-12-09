7e2f863fea677e79a4af454dd8bff5e3f7ab8e93
Daniel21Villarreal <92354716+Daniel21Villarreal@users.noreply.github.com>
2021-12-09 04:13:41 -0600

    Add files via upload

---
+++ LoteriaMexicana/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace LoteriaMexicana
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pbcartas = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbcartas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pbcartas);
+            this.groupBox1.Location = new System.Drawing.Point(561, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(163, 230);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CARTAS";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(915, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(163, 230);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "HISTORIAL";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Location = new System.Drawing.Point(730, 78);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(179, 87);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "RESTAN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(163, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Siguiente Carta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(915, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 55);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Barajear";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pbcartas
+            // 
+            this.pbcartas.Location = new System.Drawing.Point(7, 20);
+            this.pbcartas.Name = "pbcartas";
+            this.pbcartas.Size = new System.Drawing.Size(150, 204);
+            this.pbcartas.TabIndex = 0;
+            this.pbcartas.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(7, 20);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(150, 204);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(167, 58);
+            this.label1.TabIndex = 0;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 418);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbcartas)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pbcartas;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ LoteriaMexicana/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace LoteriaMexicana
+{
+    public partial class Form1 : Form
+    {
+        Stack<int> CARTAS = new Stack<int>();
+        Random random = new Random();
+        const int CANTIDAD_CARTAS = 54;
+        private PictureBox[] cartas;
+        private PictureBox[] tabla;
+        public Form1()
+        {
+            InitializeComponent();
+            cartas = new PictureBox[54];
+            tabla = new PictureBox[25];
+            inicalizarTabla();
+        }
+
+        private void inicalizarTabla()
+        {
+            int r=0, c = 0;
+
+            int[] cartas = new int[34];
+           
+
+            for (int i = 0; i <cartas.Length; i++)
+            {
+                cartas[i]= i +1;
+            }
+
+            //recorrer el arreaglo para SWAP
+            Random rnd = new Random();
+            int a, aux;
+            for (int i = 0; i < cartas.Length; i++)
+            {
+                a = rnd.Next(cartas.Length);
+                aux = cartas[i];
+                cartas[i] = cartas[a];
+                cartas[a] = aux;
+            }
+
+            for (int i = 0; i < tabla.Length; i++)
+            {
+                tabla[i] = new PictureBox();
+                tabla[i].Location = new System.Drawing.Point(100+(c*90), 25+(r*125));
+                tabla[i].Name = "picTabla"+i;
+                tabla[i].Size = new System.Drawing.Size(85, 120);
+                tabla[i].TabIndex = 0+i;
+                tabla[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                tabla[i].TabStop = false;
+                tabla[i].Image = Image.FromFile(@"cartas\"+(cartas[i])+".jpg");
+                this.Controls.Add(tabla[i]);
+                c++;
+                if (c==5)
+                {
+                    r++;
+                    c = 0;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listView1.View = View.LargeIcon;
+            this.imageList1.ImageSize = new Size(150, 220);
+            this.listView1.LargeImageList = this.imageList1;
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = (54 - cartas.Count()).ToString();
+            bool bandera = false;
+            if (cartas.Count()==54)
+            {
+                bandera = true;
+                MessageBox.Show("son todas las cartas", "AVISO", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+            }
+
+             while(!bandera)
+            {
+                int num = random.Next(1, 54);
+                if (CARTAS.Contains(num))
+                {
+                    pbcartas.Image = Image.FromFile(@"Bibliotecas\Documentos"+num+".jpg");
+                    pbcartas.SizeMode = PictureBoxSizeMode.StretchImage;
+                    this.imageList1.Images.Add(Image.FromFile(@"Bibliotecas\Documentos" + num + ".jpg"));
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = j;
+                    this.listView1.Items.Add(item);
+                    bandera = true;
+                    j++;
+
+
+
+                }
+            }
+        }
+    }
+}
